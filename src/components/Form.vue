89f43c729a3f4a427ccdbfd9bb53ba98bc6bbc07
edwardz8 <tolklorist@gmail.com>
2021-09-12 15:49:10 -0400

    form

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -8,17 +8,17 @@
     <div class="header-group items-center justify-center py-2 px-4 sm:px-2 lg:px-2">
      <h2 class="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">rotorink newsletter 🏒</h2>
     <div class="max-w-md w-full form-container">
-      <form ref="formTag" action="/" name="newsletter" data-netlify="true" class="mt-2 space-y-3" method="post">
+      <form @submit.prevent="sendForm" action="/" name="newsletter" data-netlify="true" class="mt-2 space-y-3" method="POST">
         <!-- name="newsletter-form" if name="hpfield" fails -->
         <input type="hidden" name="form-name" value="newsletter" />
         <div class="rounded-md shadow-sm">
           <div>
             <label for="email" class="sr-only">Email address</label>
-            <input id="email" name="email" type="email" autocomplete="email" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address" />
+            <input v-model="form.email" id="email" name="email" type="email" autocomplete="email" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address" />
           </div>
         </div>
         <div>
-          <button @click.prevent="sendForm" type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-gray-800 hover:bg-red-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+          <button type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-gray-800 hover:bg-red-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
             Get Newsletter
           </button>
         </div>
@@ -36,24 +36,33 @@ import axios from 'axios'
 export default {
   data() {
     return {
-      email: "",
+        form: {
+            email: "",
+        }
     };
   },
   methods: {
-      async sendForm() {
-         // document.querySelector('form').addEventListener('submit')
-            let formName = this.$refs.formTag.getAttribute('name');
-        
-            let formData = new FormData();
-            formData.append('email', this.email);
-            formData.append('form-name', formName);
-            let resp = await fetch('https://rotorink0.netlify.app/#/contact', {
-                method:'POST',
-                headers: { "Content-Type": "application/x-www-form-urlencoded" },
-                body: new URLSearchParams(formData).toString(),
-            });
-            // assume ok, although double check
-            alert('Submitted!');
+      encode(data) {
+          return Object.keys(data)
+          .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
+          .join('&')
+      },
+      sendForm() {
+          fetch('https://rotorink0.netlify.app/#/contact', {
+              method: 'POST',
+              headers: {
+                  "Content-Type": "application/x-www-form-urlencoded"
+              },
+              body: this.encode({
+                  'form-name': 'newsletter',
+                  ...this.form
+              })
+          })
+          .then(() => {
+              alert('Submitted!');
+              console.log('form submitted', )
+          })
+          .catch(e => console.error(e))
         }
   }
 };

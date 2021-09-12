b9dad85666d57589ba0e327e64ce312d039a6a8a
edwardz8 <tolklorist@gmail.com>
2021-09-12 15:26:19 -0400

    form

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+const path = require("path");
+const PrerenderSPAPlugin = require("prerender-spa-plugin");
+
+module.exports = {
+  // prerender
+  configureWebpack: {
+    plugins: [
+      new PrerenderSPAPlugin({
+        staticDir: path.join(__dirname, "dist"),
+        indexPath: path.join(__dirname, "dist", "index.html"),
+        // Required - Routes to render.
+        routes: ["/", "/success", "/fail", "/contact"],
+      }),
+    ],
+  },
+};
